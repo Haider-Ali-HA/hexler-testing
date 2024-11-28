645bd1926c146828bf5f72a2c64f6cf4d05993ee
Mehdi Ali <mehdikhanofficial@gmail.com>
2024-11-28 20:13:02 +0500

    typescript issues fixed in projects.tsx

--- main-file/components/layout/projects/Projects.tsx
+++ main-file/components/layout/projects/Projects.tsx
@@ -8,17 +8,18 @@ import { projectsData } from "@/constants/projectsData";
 import { projectServices } from "@/services/projectServices";
 import toast from "react-hot-toast";
 import Loader from "@/components/layout/Loader";
+import { Project } from "@/types/projectTypes";
 
 const Projects = () => {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [projects, setProjects] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [projects, setProjects] = useState<Project[]>([]);
   const [categories, setCategories] = useState<string[]>([]);
-  const [filteredProjects, setFilteredProjects] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
 
   const fetchProjects = async () => {
-    setLoading(true); 
+    setLoading(true);
     const result = await projectServices.getProjects();
 
     if (result.success) {
@@ -26,13 +27,13 @@ const Projects = () => {
       console.log(result.data);
 
       // Getting Categories dynamically based on fetched projects
-      const allCategories = [
+      const allCategories: (any)[] = [
         "All",
         ...Array.from(
           new Map(
             result.data
-              .flatMap((item) => item.projectCategory) // Make sure this matches your project data structure
-              .map((category) => [category, true])
+              .flatMap((item: Project) => item.projectCategory)
+              .map((category: string) => [category, true])
           ).keys(),
         ),
       ];
@@ -42,10 +43,10 @@ const Projects = () => {
       const projectsAfterFilteration =
         selectedCategory === "All"
           ? result.data
-          : result.data.filter((project) =>
-              project.projectCategory.includes(selectedCategory)
-            );
-      setFilteredProjects(projectsAfterFilteration); 
+          : result.data.filter((project: Project) =>
+            project.projectCategory.includes(selectedCategory)
+          );
+      setFilteredProjects(projectsAfterFilteration);
       setLoading(false);
     } else {
       setError(result.message);
@@ -70,56 +71,54 @@ const Projects = () => {
             </div>
           </div>
 
-          {
-            loading ? (
-              <Loader />
-            ):(
-              <>
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
               {/* Tab Buttons for Categories */}
-          <div className="category-tabs">
-            {categories.map((category, index) => (
-              <button
-                key={index}
-                className={`tab-button ${selectedCategory === category ? "active" : ""}`}
-                onClick={() => setSelectedCategory(category)}
-              >
-                {category}
-              </button>
-            ))}
-          </div>
+              <div className="category-tabs">
+                {categories.map((category, index) => (
+                  <button
+                    key={index}
+                    className={`tab-button ${selectedCategory === category ? "active" : ""}`}
+                    onClick={() => setSelectedCategory(category)}
+                  >
+                    {category}
+                  </button>
+                ))}
+              </div>
 
-          {/* Project Listings */}
-          <div className="project-listings">
-            {filteredProjects.length > 0 ? (
-              filteredProjects.map((project, index) => (
-                <Link
-                  href={project.projectLink}
-                  target="_blank"
-                  key={index}
-                  className="project-card"
-                >
-                  <Image
-                    src={project.projectImage || "https://plus.unsplash.com/premium_photo-1675793715030-0584c8ec4a13?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-                    alt={project.projectName}
-                    className="bg-image project-img"
-                    width={1200}
-                    height={800}
-                    layout="responsive"
-                  />
+              {/* Project Listings */}
+              <div className="project-listings">
+                {filteredProjects.length > 0 ? (
+                  filteredProjects.map((project: Project, index: number) => (
+                    <Link
+                      href={project.projectLink}
+                      target="_blank"
+                      key={index}
+                      className="project-card"
+                    >
+                      <Image
+                        src={project.projectImage || "https://plus.unsplash.com/premium_photo-1675793715030-0584c8ec4a13?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                        alt={project.projectName}
+                        className="bg-image project-img"
+                        width={1200}
+                        height={800}
+                        layout="responsive"
+                      />
 
-                  <div className="view-project-listing-wrapper">
-                    <h3 className="project-title">{project.projectName}</h3>
-                    <p style={{ textAlign: "center" }}>{project.projectDescription}</p>
-                  </div>
-                </Link>
-              ))
-            ) : (
-              <p>No projects available in this category.</p>
-            )}
-          </div>
-              </>
-            )
-          }
+                      <div className="view-project-listing-wrapper">
+                        <h3 className="project-title">{project.projectName}</h3>
+                        <p style={{ textAlign: "center" }}>{project.projectDescription}</p>
+                      </div>
+                    </Link>
+                  ))
+                ) : (
+                  <p>No projects available in this category.</p>
+                )}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
---
+++ main-file/types/projectTypes.ts
@@ -0,0 +1,10 @@
+export interface Project {
+    projectName: string;
+    projectTagline: string;
+    projectDescription?: string;
+    projectCategory: string;
+    projectImage?: string;
+    projectLink: string;
+    projectOrder: number;
+  }
+  
